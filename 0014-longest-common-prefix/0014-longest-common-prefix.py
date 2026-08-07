1cd664bd45f0633296dcbc54295825d756a719e9
nehakonar01-rgb <nehakonar01@gmail.com>
2026-08-07 22:36:18 +0530

    Time: 0 ms (100%), Space: 12.3 MB (69.6%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+
+        if len(strs) == 0:
+            return ""
+
+        for i in range(len(strs[0])):
+
+            ch = strs[0][i]
+
+            for j in range(1, len(strs)):
+
+                if i >= len(strs[j]) or strs[j][i] != ch:
+                    return strs[0][:i]
+
+        return strs[0]
+        
